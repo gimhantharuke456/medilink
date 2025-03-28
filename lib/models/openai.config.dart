efbe89d2081cedd08b77dde80fcff3186c9d04a5
gimhantharuke456 <gimhantharuke456@gmail.com>
2025-03-28 07:13:17 +0530

    key removed

--- lib/models/openai.config.dart
+++ lib/models/openai.config.dart
@@ -1,6 +1,5 @@
 class OpenAIConfig {
-  static const String apiKey =
-      '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';
+  static const String apiKey = '';
   static const String baseUrl = 'https://api.openai.com/v1';
   static const String gpt4VisionModel = 'gpt-4o';
   static const String gpt3Model = 'gpt-3.5-turbo';
